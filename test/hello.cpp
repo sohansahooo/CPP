4270b80e2eadb18ba5197cf45e1cfa13af11df99
sohanxsahoo <sohanxsahoo@gmail.com>
2024-09-02 09:59:54 +0530

    part2

--- test/hello.cpp
+++ test/hello.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-using namespace std;
+// using namespace std;
+/*using std::cout;
+using std::endl;*/
 
 int main(){
-    cout<< "Hello, World!";
+    // cout << "Hello World";
+    /* cout << "Hello World" << endl; */
+    // std::cout<< "Hello, World!" << std::endl;
+    return 0;
 }
